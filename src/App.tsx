5bead3b8daf990b70434eb71e39322564c1505c3
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-08-03 18:15:13 +0300

    Removing unnecessary Vite auto-generated content

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a style={{ display: 'block' }} href="/594a251b1">
+          <img
+            style={{ background: 'black' }}
+            alt="Fintraffic Design System"
+            src="https://zeroheight.s3.eu-west-1.amazonaws.com/zeroheight-huddle/logos/92790/92456/logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIA3AVNYHQKTB7DNOEL%2F20230803%2Feu-west-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20230803T144423Z&amp;X-Amz-Expires=86400&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=55bb54e19f8a83e468a86dd45a14b20687d156899aa1ce5e4a8f65e87800a7ce"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}> count is {count}. </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1>VACO UI</h1>
+      <div>Yay</div>
     </>
   )
 }
