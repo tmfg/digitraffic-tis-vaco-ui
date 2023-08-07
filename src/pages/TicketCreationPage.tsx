050c6e7dcae3c871ded2a049c6002013e0d38c38
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-08-07 14:16:42 +0300

    Request ticket creation example

--- src/pages/TicketCreationPage.tsx
+++ src/pages/TicketCreationPage.tsx
@@ -1,9 +1,43 @@
-const TicketCreationPage = () => {
-  return (
-    <>
-      <h1>Create ticket</h1>
+import { useState } from 'react'
+import { HttpClient } from '../HttpClient'
 
+const TicketCreationPage = () => {
+  const [creationResponse, setCreationResponse] = useState({})
+
+  const yikes = async () => {
+    const requestBody = {
+      url: 'http://localhost:8080/stuff.zip',
+      format: 'gtfs',
+      businessId: '2942108-7',
+      etag: 'etagg',
+      validation: {},
+      metadata: {
+        brewingEquipment: 'teapot',
+        'capacity (ml)': 1500
+      }
+    }
+
+    const response = await HttpClient.post('/queue', requestBody)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    setCreationResponse(response.data)
+  }
+
+  return (
+    <div>
+      <h1>Create ticket</h1>
+      {creationResponse && Object.keys(creationResponse).length > 0 && (
+        <div
+          style={{
+            fontFamily: 'PublicSans-Medium',
+            marginLeft: 25
+          }}
+        >
+          <h2>Submitted! </h2>
+          <pre style={{ width: 600, whiteSpace: 'pre-wrap' }}>{JSON.stringify(creationResponse)}</pre>
+        </div>
+      )}
       <button
+        onClick={yikes}
         style={{
           marginTop: 25,
           fontFamily: 'PublicSans-Medium',
@@ -17,9 +51,9 @@ const TicketCreationPage = () => {
           cursor: 'pointer'
         }}
       >
-        Launch!
+        Launch {Object.keys(creationResponse).length > 0 && 'again'}!
       </button>
-    </>
+    </div>
   )
 }
 
