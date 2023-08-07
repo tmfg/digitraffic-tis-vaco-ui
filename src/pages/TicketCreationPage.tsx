daef24e63312de16a27cfe0b4e188b0d8684607b
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-08-07 10:29:24 +0300

    Add a button to launch a request to VACO backend

--- src/pages/TicketCreationPage.tsx
+++ src/pages/TicketCreationPage.tsx
@@ -1,5 +1,26 @@
 const TicketCreationPage = () => {
-  return <h1>Create ticket</h1>
+  return (
+    <>
+      <h1>Create ticket</h1>
+
+      <button
+        style={{
+          marginTop: 25,
+          fontFamily: 'PublicSans-Medium',
+          backgroundColor: '#0034ac',
+          color: 'white',
+          width: 125,
+          marginLeft: 25,
+          paddingTop: 18,
+          paddingBottom: 18,
+          fontSize: '1.2rem',
+          cursor: 'pointer'
+        }}
+      >
+        Launch!
+      </button>
+    </>
+  )
 }
 
 export default TicketCreationPage
