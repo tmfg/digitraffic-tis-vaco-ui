d342e656546effda4c12b2109d38c6e6ba73f1de
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-10-03 10:19:39 +0300

    Make preview mode 5173

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import pluginChecker from 'vite-plugin-checker'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginChecker({ typescript: true })],
-  base: '/ui'
+  base: '/ui',
+  preview: {
+    port: 5173
+  }
 })
