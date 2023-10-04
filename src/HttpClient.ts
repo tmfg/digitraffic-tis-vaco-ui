4520d6b27c164101d32e907017fdde9446464ae0
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-10-04 12:53:26 +0300

    Fix API url for cloud environment

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const TIS_ENVIRONMENT = import.meta.env.DEV // change this one we actually have env variables...
 export const VACO_URL = TIS_ENVIRONMENT
   ? 'http://localhost:8080'
-  : 'https://digitraffic-tis-dev.aws.fintraffic.cloud/api/'
+  : 'https://digitraffic-tis-dev.aws.fintraffic.cloud'
 
 /*export const headers = {
   'Access-Control-Allow-Origin': '*'
