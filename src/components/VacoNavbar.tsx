e2c3e1b92f07d71071c0573432c7a8d9a141cc6c
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-10-10 17:32:37 +0300

    Revert back to design system usage

--- src/components/VacoNavbar.tsx
+++ src/components/VacoNavbar.tsx
@@ -1,6 +1,72 @@
-const VacoNavbar = () => {
+import {
+  FdsNavigationItemPosition,
+  FdsNavigationVariant
+} from '@fintraffic-design/coreui-components/src/fds-navigation'
+import Navbar from './Navbar'
+import { FdsNavigationItem } from '@fintraffic-design/coreui-components/src/fds-navigation'
+import { useMsal } from '@azure/msal-react'
+import { useEffect, useState } from 'react'
 
-  return <div> App Navbar </div>
+export const vacoStaticNavbarItems: FdsNavigationItem[] = [
+  {
+    label: 'VACO',
+    value: '/'
+  },
+  {
+    label: 'Dashboard',
+    value: '/dashboard'
+  },
+  {
+    label: 'Create ticket',
+    value: '/ticket/request'
+  },
+  {
+    label: 'View ticket',
+    value: '/ticket/info'
+  }
+]
+
+const userNavbarItem = {
+  label: '',
+  value: '',
+  position: FdsNavigationItemPosition.right
+}
+
+const languageNavbarItem = {
+  label: 'In English',
+  value: '',
+  position: FdsNavigationItemPosition.right
+}
+
+const VacoNavbar = () => {
+  const { instance } = useMsal()
+  const [userName, setUserName] = useState<string | undefined>(undefined)
+  const vacoNavbarItems = [...vacoStaticNavbarItems]
+
+  useEffect(() => {
+    const account = instance.getActiveAccount()
+    if (account && !userName) {
+      setUserName(account.name)
+    }
+  }, [instance])
+
+  useEffect(() => {
+    // Later: getting the language selection from local storage or wherever
+    // Once everything is ready, time to add all "dynamic" navbar items
+    if (userName) {
+      userNavbarItem.label = userName
+      vacoNavbarItems.push(userNavbarItem)
+      vacoNavbarItems.push(languageNavbarItem)
+    }
+  }, [userName])
+
+  return (
+    <Navbar
+      variant={FdsNavigationVariant.secondary}
+      items={userName ? vacoNavbarItems : vacoStaticNavbarItems}
+      barIndex={1}
+    />
+  )
 }
 
 export default VacoNavbar
--- src/pages/TicketCreationPage.tsx
+++ src/pages/TicketCreationPage.tsx
@@ -3,6 +3,9 @@ import { getHeaders, HttpClient } from '../HttpClient'
 import { EntryRequest } from '../types/EntryRequest'
 import { EntryResource } from '../types/EntryResource'
 import { Link } from 'react-router-dom'
+import { FdsButtonComponent } from '../components/fds/FdsButtonComponent'
+import { FdsNavigationItem } from '@fintraffic-design/coreui-components/src/fds-navigation'
+import { vacoStaticNavbarItems } from '../components/VacoNavbar'
 import { useMsal } from '@azure/msal-react'
 import { acquireToken } from '../hooks/auth'
 
@@ -45,12 +48,30 @@ const TicketCreationPage = () => {
         <div>
           <h4>
             Submitted! Ticket:
-            <Link to={'/ticket/info/' + entryResource.data.publicId}>{entryResource.data.publicId}</Link>
+            <Link
+              onClick={() => {
+                // VACO navbar should update selected menu item:
+                const element = document.getElementsByTagName('fds-navigation-test')[1]
+                element?.dispatchEvent(
+                  new CustomEvent<FdsNavigationItem>('externalNavigation', {
+                    detail: vacoStaticNavbarItems[3],
+                    bubbles: true
+                  })
+                )
+              }}
+              to={'/ticket/info/' + entryResource.data.publicId}
+            >
+              {entryResource.data.publicId}
+            </Link>
           </h4>
           <pre style={{ width: 700, whiteSpace: 'pre-wrap' }}>{JSON.stringify(entryResource)}</pre>
         </div>
       )}
-      <button onClick={submitTicket}>{`Launch${entryResource ? ' again' : ''}!`}</button>
+      <FdsButtonComponent
+        icon={'alert-circle'}
+        onClick={submitTicket}
+        label={`Launch${entryResource ? ' again' : ''}!`}
+      />
     </div>
   )
 }
