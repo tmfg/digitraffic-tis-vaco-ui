add8386b0d9d46f8f0b8bb9b5300838937a1e9c0
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2024-03-15 10:15:11 +0200

    Data delivery CSV export

--- src/pages/AdminTools/DataDeliveryPage.tsx
+++ src/pages/AdminTools/DataDeliveryPage.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next'
-import { InteractionStatus } from '@azure/msal-browser'
-import { acquireToken } from '../../hooks/auth'
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useIsAuthenticated, useMsal } from '@azure/msal-react'
+import { acquireToken, useAcquireToken } from '../../hooks/auth'
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react'
 import { getHeaders, HttpClient } from '../../HttpClient'
 import { CompanyLatestEntryResource } from '../../types/DataDelivery'
 import { AppContext, AppContextType } from '../../AppContextProvider'
@@ -10,13 +9,19 @@ import AuthRequiredPage from '../Error/AuthRequiredPage'
 import AdminRoleRequiredPage from '../Error/AdminRoleRequiredPage'
 import DataDeliveryView from '../../components/DataDelivery/DataDeliveryView'
 import { useContext, useEffect, useState } from 'react'
+import { FdsButtonVariant } from '../../../coreui-components/src/fds-button'
+import { FdsButtonComponent } from '../../components/fds/FdsButtonComponent'
+import { FdsTokenSize2 } from '../../../coreui-css/lib'
+import { downloadFile } from '../../util/download'
+import { getCurrentTimestamp } from '../../util/date'
+import { AxiosResponse } from 'axios'
 
 const DataDeliveryPage = () => {
   const { t } = useTranslation()
-  const { instance, inProgress } = useMsal()
-  const isAuthenticated = useIsAuthenticated()
-  const [accessToken, setAccessToken] = useState<string | null>(null)
+  const { i18n } = useTranslation()
+  const [accessToken] = useAcquireToken()
   const appContext: AppContextType = useContext(AppContext)
+  const { instance, inProgress } = useMsal()
   const [hasAdminRole, setHasAdminRole] = useState<boolean | undefined>(undefined)
   const [hasCompanyAdminRole, setHasCompanyAdminRole] = useState<boolean | undefined>(undefined)
   const [data, setData] = useState<CompanyLatestEntryResource[] | null>(null)
@@ -27,29 +32,6 @@ const DataDeliveryPage = () => {
       setHasCompanyAdminRole(rolesContainVacoCompanyAdmin(appContext.roles))
     }
   }, [appContext])
-
-  useEffect(() => {
-    let ignore = false
-
-    if (inProgress === InteractionStatus.None && isAuthenticated && !ignore && !accessToken) {
-      acquireToken(instance, inProgress).then(
-        (tokenResult) => {
-          if (!tokenResult) {
-            // TODO: At some point, show some error notification
-            return
-          }
-          setAccessToken(tokenResult.accessToken)
-        },
-        (error) => {
-          // TODO: show alert
-          return Promise.reject(error)
-        }
-      )
-    }
-    return () => {
-      ignore = true
-    }
-  }, [instance, inProgress, accessToken, isAuthenticated])
 
   useEffect(() => {
     let ignore = false
@@ -69,10 +51,57 @@ const DataDeliveryPage = () => {
     }
   }, [accessToken])
 
+  const handleDownloadedCsv = (response: AxiosResponse<any>) => {
+    if (!response?.data) {
+      return
+    }
+    downloadFile(
+      response.data as string,
+      `${t('admin:dataDelivery:header')} ${getCurrentTimestamp()}.csv`,
+      response.data.type
+    )
+  }
+
   return (
     <div className={'page-content'}>
       <AuthenticatedTemplate>
-        <h1>{t('admin:dataDelivery:header')}</h1>
+        <div style={{ display: 'flex', marginBottom: 0, alignItems: 'center', justifyContent: 'space-between' }}>
+          <h1>{t('admin:dataDelivery:header')}</h1>
+          <FdsButtonComponent
+            onClick={() => {
+              acquireToken(instance, inProgress).then(
+                (tokenResult) => {
+                  if (!tokenResult) {
+                    // TODO: At some point, show some error notification
+                    return
+                  }
+                  HttpClient.get(
+                    '/api/ui/admin/data-delivery/export?language=' + (i18n.resolvedLanguage || i18n.language),
+                    {
+                      ...getHeaders(tokenResult.accessToken),
+                      responseType: 'blob'
+                    }
+                  ).then(
+                    (response) => handleDownloadedCsv(response),
+                    (error) => {
+                      // TODO: show alert
+                      return Promise.reject(error)
+                    }
+                  )
+                },
+                (error) => {
+                  // TODO: show alert
+                  return Promise.reject(error)
+                }
+              )
+            }}
+            slot="separated"
+            icon="download"
+            iconSize={FdsTokenSize2}
+            variant={FdsButtonVariant.secondary}
+            label={'Export CSV'}
+          />
+        </div>
         {(hasAdminRole || hasCompanyAdminRole) && <DataDeliveryView data={data} />}
         {hasAdminRole !== undefined && hasCompanyAdminRole !== undefined && !(hasAdminRole || hasCompanyAdminRole) && (
           <AdminRoleRequiredPage />
--- src/util/date.ts
+++ src/util/date.ts
@@ -11,3 +11,14 @@ export const formatDate = (timestamp: string | null) => {
     timeZone: 'Europe/Helsinki'
   }).format(new Date(timestamp))
 }
+
+export const getCurrentTimestamp = () => {
+  return new Intl.DateTimeFormat('fi', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'Europe/Helsinki'
+  }).format(new Date())
+}
