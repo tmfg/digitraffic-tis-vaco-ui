1392afbb39d0182ca4330535df8af11a4715be7e
Esko Suomi <esko.suomi@solita.fi>
2024-04-02 14:25:47 +0300

    remove vaco.user from UI related calls

--- src/util/role.ts
+++ src/util/role.ts
@@ -2,10 +2,6 @@ export const rolesContainVacoAdmin = (roles: string[]) => {
   return roles.includes('vaco.admin')
 }
 
-export const rolesContainVacoUser = (roles: string[]) => {
-  return roles.includes('vaco.user')
-}
-
 export const rolesContainVacoCompanyAdmin = (roles: string[]) => {
   return roles.includes('vaco.company_admin')
 }
