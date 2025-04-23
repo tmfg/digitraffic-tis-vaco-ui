72ff5d6f502dc45eca2c17d6edcbdf5f77d378fa
Areta Santos <areta.santos@solita.fi>
2025-04-23 09:18:50 +0300

    Add tables and all statisticsFigures to statisticspage to be able to show all wanted data

--- src/pages/AdminTools/Statistics/StatisticsPage.tsx
+++ src/pages/AdminTools/Statistics/StatisticsPage.tsx
@@ -1,35 +1,149 @@
-import StatisticsFigure from '../../../components/Statistics/StatisticsFigure.tsx'
+import EntryStatisticsFigure from '../../../components/Statistics/EntryStatisticsFigure.tsx'
 import { useStatisticsFetch } from './hooks.ts'
 import { useAcquireToken } from '../../../hooks/auth.ts'
 import { useTranslation } from 'react-i18next'
 import { AuthenticatedTemplate } from '@azure/msal-react'
-import { StatResource } from '../../../types/Statistics.ts'
+import EntryInputStatisticsFigure from '../../../components/Statistics/EntryInsnputStatisticsFigure.tsx'
+import Table, { HeaderItem, TableItem } from '../../../components/Common/Table/Table.tsx'
 import { useEffect, useState } from 'react'
+import { StatResource } from '../../../types/Statistics.ts'
+import TaskStatisticsFigure from '../../../components/Statistics/TaskStatisticsFigure.tsx'
+import Section from '../../../components//Common/Section/Section.tsx'
 
 const StatisticsPage = () => {
 
   const [accessToken] = useAcquireToken()
-  const [statusStats] = useStatisticsFetch(accessToken)
+  const [entryStatusStats, taskStatusStats] = useStatisticsFetch(accessToken)
   const { t } = useTranslation()
-  const [filteredStats30Days, setFilteredStats30Days] = useState<StatResource[] | null>(null);
+  const [entryRowItems, setEntryRowItems] = useState<TableItem[][]>([])
+  const [taskRowItems, setTaskRowItems] = useState<TableItem[][]>([])
+  const [inputRowItems, setInputRowItems] = useState<TableItem[][]>([])
+  const [entryInputGroupedData, setEntryInputGroupedData] = useState<Record<string, Record<string, number>>>({});
+  const [taskGroupedData, setTaskGroupedData] = useState<Record<string, Record<string, number>>>({});
+  const [entryStatusGroupedData, setEntryStatusGroupedData] = useState<Record<string, Record<string, number>>>({});
 
-  const isInLast30Days = (timestamp: string): boolean => {
-    const today = new Date();
-    const entryDate = new Date(timestamp);
-    const diffTime = today.getTime() - entryDate.getTime();
-    const diffDays = diffTime / (1000 * 3600 * 24);
-    return diffDays <= 30;
-  };
+  const statusHeaderItems:HeaderItem[] = [
+    {
+      name: 'status',
+      value: t('admin:statistics:summary:EntryStatus')
+    },
+    {
+      name: 'count',
+      value: t('admin:statistics:summary:count')
+    }
+  ]
+
+  const taskHeaderItems:HeaderItem[] = [
+    {
+      name: 'name',
+      value: t('admin:statistics:summary:TaskName')
+    },
+    {
+      name: 'count',
+      value: t('admin:statistics:summary:count')
+    }
+  ]
+
+  const InputFormatHeaderItems:HeaderItem[] = [
+    {
+      name: 'name',
+      value: t('admin:statistics:summary:InputFormatName')
+    },
+    {
+      name: 'count',
+      value: t('admin:statistics:summary:count')
+    }
+  ]
 
   useEffect(() => {
-    if (statusStats) {
-      const filtered30Days = statusStats.filter((entry: StatResource) =>
-        isInLast30Days(entry.data.timestamp)
-      )
-      setFilteredStats30Days(filtered30Days);
-    }
+    if (entryStatusStats && taskStatusStats) {
 
-    }, [statusStats]);
+      const inputFormatGroupedData = GroupDataByName(entryStatusStats)
+      setEntryInputGroupedData(inputFormatGroupedData);
+      const taskGroupedData = GroupDataByName(taskStatusStats)
+      setTaskGroupedData(taskGroupedData)
+      const entryGroupedData = GroupDataByStatus(entryStatusStats)
+      setEntryStatusGroupedData(entryGroupedData)
+
+      const today = new Date().toISOString().split('T')[0];
+      const todayTaskStats = taskGroupedData[today] || {};
+      const todayInputFormatStats = inputFormatGroupedData[today] || {};
+      const todayEntryStatusStats = entryGroupedData[today] || {};
+
+      const taskFilteredRows = Object.entries(todayTaskStats).map(([name, count]) => ([
+        {
+          name: 'name',
+          value: name,
+          colSpan: 1,
+          plainValue: name
+        },
+        {
+          name: 'count',
+          value: count,
+          colSpan: 1,
+          plainValue: count
+        }
+      ]));
+      const entryFilteredRows = Object.entries(todayEntryStatusStats).map(([statusName, count]) => ([
+        {
+          name: 'status',
+          value: statusName,
+          colSpan: 1,
+          plainValue: statusName
+        },
+        {
+          name: 'count',
+          value: count,
+          colSpan: 1,
+          plainValue: count
+        }
+      ]));
+
+      const inputFilteredRowsFilteredRows = Object.entries(todayInputFormatStats).map(([name, count]) => ([
+        {
+          name: 'name',
+          value: name,
+          colSpan: 1,
+          plainValue: name
+        },
+        {
+          name: 'count',
+          value: count,
+          colSpan: 1,
+          plainValue: count
+        }
+      ]));
+
+      setEntryRowItems(entryFilteredRows);
+      setTaskRowItems(taskFilteredRows)
+      setInputRowItems(inputFilteredRowsFilteredRows)
+    }
+  }, [entryStatusStats, taskStatusStats]);
+
+  const GroupDataByName = (stats: StatResource[]) => {
+
+    if (stats) {
+      return stats.reduce((allForDay, oneStat) => {
+        const dateStr = oneStat.data.timestamp
+        allForDay[dateStr] = allForDay[dateStr] || {}
+        allForDay[dateStr][oneStat.data.name] = (allForDay[dateStr][oneStat.data.name] || 0) + oneStat.data.count
+        return allForDay
+      }, {} as Record<string, Record<string, number>>)
+    }
+    return {};
+  }
+
+  const GroupDataByStatus = (stats: StatResource[]) => {
+    if (stats) {
+      return stats.reduce((allForDay, oneStat) => {
+        const dateStr = oneStat.data.timestamp;
+        allForDay[dateStr] = allForDay[dateStr] || {};
+        allForDay[dateStr][oneStat.data.status] = (allForDay[dateStr][oneStat.data.status] || 0) + oneStat.data.count;
+        return allForDay;
+      }, {} as Record<string, Record<string, number>>);
+    }
+    return {};
+  }
 
   return (
     <div className={'page-content'}>
@@ -37,8 +151,38 @@ const StatisticsPage = () => {
         <div>
           <h1>{t('admin:statistics:header')}</h1>
           <div style={{ display: 'flex', width: '100%' }}>
-            <div style={{ flex: 1, }}>
-              <StatisticsFigure stats={filteredStats30Days} />
+            <div style={{ flex: 1 }}>
+              <Section titleKey={t('admin:statistics:summary:todayStatusHeader')} hidable={true}>
+              <div style={{ display: 'flex', width: '100%', flexWrap: 'wrap', gap: '20px' }}>
+                <div style={{ flex: 1, maxWidth: '33%', marginBottom: '20px' }}>
+                  <Table
+                    tableTitle={'statistics'}
+                    headerItems={statusHeaderItems}
+                    rows={entryRowItems}
+                    isHalfWide={true}
+                  />
+                </div>
+                <div style={{ flex: 1, maxWidth: '33%', marginBottom: '20px' }}>
+                  <Table
+                    tableTitle={'taskStatistics'}
+                    headerItems={taskHeaderItems}
+                    rows={taskRowItems}
+                    isHalfWide={true}
+                  />
+                </div>
+                <div style={{ flex: 1, maxWidth: '33%', marginBottom: '20px' }}>
+                  <Table
+                    tableTitle={'inputStatistics'}
+                    headerItems={InputFormatHeaderItems}
+                    rows={inputRowItems}
+                    isHalfWide={true}
+                  />
+                </div>
+              </div>
+              </Section>
+              <EntryStatisticsFigure groupedData={entryStatusGroupedData}/>
+              <TaskStatisticsFigure groupedData={taskGroupedData}/>
+              <EntryInputStatisticsFigure groupedData={entryInputGroupedData} />
             </div>
           </div>
         </div>
